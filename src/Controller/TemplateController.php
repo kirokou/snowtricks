0a1ea9743e8ba4a64e1e22cc96f46955d45b0382
kirikou <borgine@hotmail.fr>
2020-02-06 21:27:41 +0100

    feat: make template and templateController
    Integration of bootstrap materaliz template.
    making of templateController

---
+++ src/Controller/TemplateController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TemplateController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('template/index.html.twig', [
+            'controller_name' => 'TemplateController',
+        ]);
+    }
+
+    /**
+     * @Route("/template/show", name="Show")
+     */
+    public function show()
+    {
+        return $this->render('template/show.html.twig', [
+            'controller_name' => 'TemplateController',
+        ]);
+    }
+
+     /**
+     * @Route("/template/edit", name="edit")
+     */
+    public function edit()
+    {
+        return $this->render('template/edit.html.twig', [
+            'controller_name' => 'TemplateController',
+        ]);
+    }
+
+     /**
+     * @Route("/template/forgot", name="forgot")
+     */
+    public function forgot()
+    {
+        return $this->render('template/forgot.html.twig', [
+            'controller_name' => 'TemplateController',
+        ]);
+    }
+
+     /**
+     * @Route("/template/login", name="login")
+     */
+    public function login()
+    {
+        return $this->render('template/login.html.twig', [
+            'controller_name' => 'TemplateController',
+        ]);
+    }
+
+     /**
+     * @Route("/template/reset", name="reset")
+     */
+    public function reset()
+    {
+        return $this->render('template/reset.html.twig', [
+            'controller_name' => 'TemplateController',
+        ]);
+    }
+
+     /**
+     * @Route("/template/registration", name="registration")
+     */
+    public function registration()
+    {
+        return $this->render('template/registration.html.twig', [
+            'controller_name' => 'TemplateController',
+        ]);
+    }
+
+}
