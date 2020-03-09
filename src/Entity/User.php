8707f45e335a32a79d3ac268a91a44319471794e
kirikou <borgine@hotmail.fr>
2020-03-09 13:46:16 +0100

    feat(avatar): udpate User to set avatar

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -62,6 +62,11 @@ class User implements UserInterface
      */
     private $reset_token;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $avatar;
+
     public function __construct()
     {
         $this->tricks = new ArrayCollection();
@@ -221,6 +226,18 @@ class User implements UserInterface
     public function setResetToken(?string $reset_token): self
     {
         $this->reset_token = $reset_token;
+
+        return $this;
+    }
+
+    public function getAvatar(): ?string
+    {
+        return $this->avatar;
+    }
+
+    public function setAvatar(?string $avatar): self
+    {
+        $this->avatar = $avatar;
 
         return $this;
     }
